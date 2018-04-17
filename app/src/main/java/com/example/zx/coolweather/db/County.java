52ee82d55a80160eed4cbb8a438ec5cd059beb8f
Virtual-Rain <563360425@qq.com>
2018-04-17 15:16:42 +0800

    完成遍历省市县三级列表功能，主要问题是实体映射名称弄错

--- app/src/main/java/com/example/zx/coolweather/db/County.java
+++ app/src/main/java/com/example/zx/coolweather/db/County.java
@@ -6,9 +6,12 @@ import org.litepal.crud.DataSupport;
  * Created by zx on 2018/4/12.
  */
 
-public class Country extends DataSupport {
+public class County extends DataSupport {
     private int id;
-    private String countryName;
+    private String countyName;
+    private String weatherId;
+    private int cityId;
+
 
     public int getId() {
         return id;
@@ -18,12 +21,12 @@ public class Country extends DataSupport {
         this.id = id;
     }
 
-    public String getCountryName() {
-        return countryName;
+    public String getCountyName() {
+        return countyName;
     }
 
-    public void setCountryName(String countryName) {
-        this.countryName = countryName;
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
     }
 
     public String getWeatherId() {
@@ -42,6 +45,4 @@ public class Country extends DataSupport {
         this.cityId = cityId;
     }
 
-    private String weatherId;
-    private int cityId;
 }
---
+++ app/src/main/java/com/example/zx/coolweather/util/HttpUtil.java
@@ -0,0 +1,16 @@
+package com.example.zx.coolweather.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by zx on 2018/4/12.
+ */
+
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address,okhttp3.Callback callback) {
+        OkHttpClient client=new OkHttpClient();
+        Request request=new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
